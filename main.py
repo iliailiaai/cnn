730dacf7346d0e945b5d032910c792e6b0597e61
iliailiaai <131950015+iliailiaai@users.noreply.github.com>
2024-12-12 20:32:05 +0300

    Update and rename main to main.py

---
+++ main.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.optimizers import Adam
+import os
+import warnings
+from tqdm import tqdm
+import keras
+from keras.layers import Dense, Dropout, LeakyReLU, BatchNormalization
+from keras.utils import to_categorical
+from tensorflow.keras.layers import AveragePooling2D
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.initializers import HeNormal
+from tensorflow.keras.callbacks import LearningRateScheduler, Callback
+from tensorflow.keras.callbacks import EarlyStopping
+# Suppress all Python warnings
+warnings.filterwarnings('ignore')
+
+# Set TensorFlow log level to suppress warnings and info messages
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # 0 = all logs, 1 = filter out INFO, 2 = filter out INFO and WARNING, 3 = ERROR only
+from tensorflow.keras.models import load_model
+
+
+
+
+import dropbox
+from keras.models import load_model
+from io import BytesIO
+
+# Ваши токены и ключи
+app_key = 'dsx3dqarmepo90n'
+app_secret = 'xjpp3t07ngawhnn'
+refresh_token = 'pkuWKOE7o1EAAAAAAAAAASmPubAWni0Rke_ttA-mDasWr-E92LQ-M6MoScoeci8L'
+
+# Создаем клиент Dropbox с токеном обновления
+dbx = dropbox.Dropbox(
+    app_key=app_key,
+    app_secret=app_secret,
+    oauth2_refresh_token=refresh_token
+)
+
+# Проверяем доступ к аккаунту
+try:
+    account_info = dbx.users_get_current_account()
+    print(account_info)
+except dropbox.exceptions.AuthError as e:
+    print(f"Ошибка аутентификации: {e}")
+    exit()
+
+# Путь к файлу на Dropbox
+folder_path = "/test/cnn_cifar100_model_67.keras"
+
+# Загружаем файл из Dropbox
+try:
+    metadata, response = dbx.files_download(folder_path)
+    print(f"Файл {metadata.name} успешно загружен.")
+    
+    # Считываем файл из памяти и загружаем модель
+    file_data = BytesIO(response.content)
+    model = load_model(file_data)
+    print("Модель успешно загружена.")
+except dropbox.exceptions.ApiError as e:
+    print(f"Ошибка при загрузке файла: {e}")
+except Exception as e:
+    print(f"Ошибка при загрузке модели: {e}")
+
+    
+    
+
+fine_labels = [
+    "яблоко", "аквариумная рыбка", "младенец", "медведь", "бобр", "кровать", "пчела", "жук", "велосипед", "бутылка",
+    "миска", "мальчик", "мост", "автобус", "бабочка", "верблюд", "банка", "замок", "гусеница", "скот", "стул",
+    "шимпанзе", "часы", "облако", "таракан", "диван", "краб", "крокодил", "чашка", "динозавр", "дельфин",
+    "слон", "камбала", "лес", "лиса", "девочка", "хомяк", "дом", "кенгуру", "клавиатура", "лампа",
+    "газонокосилка", "леопард", "лев", "ящерица", "лобстер", "мужчина", "клен", "мотоцикл", "гора",
+    "мышь", "гриб", "дуб", "апельсин", "орхидея", "выдра", "пальма", "груша", "пикап", "сосна", "равнина",
+    "тарелка", "мак", "дикобраз", "опоссум", "кролик", "енот", "скат", "дорога", "ракета", "роза", "море",
+    "тюлень", "акула", "бурозубка", "скунс", "небоскреб", "улитка", "змея", "паук", "белка", "трамвай",
+    "подсолнух", "сладкий перец", "стол", "танк", "телефон", "телевизор", "тигр", "трактор", "поезд", "форель",
+    "тюльпан", "черепаха", "гардероб", "кит", "ива", "волк", "женщина", "червь"
+]
+
+import telebot
+from telebot import types
+
+from telebot import TeleBot, types
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+# Создаем экземпляр бота
+
+#bot = telebot.TeleBot('7552516692:AAGS_5cgcQS3whi3aJ9sb3RAiZ-hhLTo5Hc')
+bot = telebot.TeleBot('7943117804:AAFY3_YKcfKAxeV3jZqM9e6RNZLgvBWxyeU')
+
+@bot.message_handler(commands=['start'])
+def start(msg: types.Message):
+
+    bot.send_message(
+        chat_id=msg.chat.id,
+        text=f"Привет, {msg.from_user.first_name}!\n\nДанный бот содержит ИИ на конволюционных сверточных сетях (CNN), способный распознавать до 100 типов объектов со средней точностью 67%.",
+    )
+
+    bot.send_message(
+    chat_id=msg.chat.id,
+    text="Для тестирования отправь любую картинку.",
+    )
+
+import telebot
+import numpy as np
+from PIL import Image
+from io import BytesIO
+
+# Предобработка изображения
+def preprocess_image(image_bytes):
+    image = Image.open(BytesIO(image_bytes))
+
+    # Изменение размера до 32x32
+    image_resized = image.resize((32, 32))
+
+    # Нормализация пикселей
+    image_array = np.array(image_resized).astype('float32') / 255.0
+
+    # Проверка на черно-белое изображение
+    if image_array.ndim == 2 or image_array.shape[-1] != 3:
+        image_array = np.stack((image_array,) * 3, axis=-1)
+
+    # Добавление измерения батча
+    return np.expand_dims(image_array, axis=0)
+
+# Обработчик изображений
+@bot.message_handler(content_types=['photo'])
+def handle_image(msg: telebot.types.Message):
+    # Получение файла изображения
+    file_info = bot.get_file(msg.photo[-1].file_id)
+    file = bot.download_file(file_info.file_path)
+
+    # Предобработка изображения
+    image_input = preprocess_image(file)
+
+    # Прогон через модель
+    prediction = model.predict(image_input)
+    predicted_class = np.argmax(prediction)
+    predicted_label = fine_labels[predicted_class]
+
+    # Отправка результата пользователю
+    bot.send_message(chat_id=msg.chat.id, text=f"На фото: *{predicted_label}.*\n\nДля повторного тестирования отправь любую картинку еще раз.", parse_mode='Markdown')
+
+
+# Обработчик ошибок
+@bot.message_handler(content_types=['text'])
+def handle_text(msg: telebot.types.Message):
+    bot.send_message(chat_id=msg.chat.id, text="Это текст, отправь изображение для распознавания.")
+
+bot.polling(none_stop=True, interval=1)
